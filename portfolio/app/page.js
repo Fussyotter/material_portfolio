a61abe26b2251f3f1a68af077f41a30bd293f882
ialoysious <isaacaloysious@gmail.com>
2023-05-24 15:28:26 -0700

    not working

---
+++ portfolio/app/page.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello World</h1>
+      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    </div>
+  )
+  
+}
